8ad24b8ca2ded9a01c7aa1bfa8dd0a271e33af25
王韬凯 <wangtaokai@smartcinema.com.cn>
2018-09-21 01:02:58 +0800

    启动一个服务返回静态页面

---
+++ server/tpl/normal.js
@@ -0,0 +1,19 @@
+module.exports = `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>koa</title>
+	</head>
+	<body>
+		<div class="con">
+			<div class="row">
+				<div class="col-md-8">
+					<h1>wangtaokai</h1>
+					<p>是个练习demo</p>
+				</div>
+			</div>
+		</div>
+	</body>
+	</html>
+`
